6d0c9c3ca0c278a350f5cbab8e1438f6e20742bf
ahmedzakaria2003 <you@example.com>
2024-12-23 01:27:08 +0200

    q5 is solved

--- Assignment-5-CSharp/Program.cs
+++ Assignment-5-CSharp/Program.cs
@@ -45,12 +45,23 @@
 
             #endregion
             #region q4
-            Console.WriteLine("enter first int");
-            int number01 = int.Parse(Console.ReadLine());
-            Console.WriteLine("enter second int");
-            int number02 = int.Parse(Console.ReadLine());
-            double result = Math.Pow(number01, number02);
-            Console.WriteLine($"{result}");
+            //Console.WriteLine("enter first int");
+            //int number01 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("enter second int");
+            //int number02 = int.Parse(Console.ReadLine());
+            //double result = Math.Pow(number01, number02);
+            //Console.WriteLine($"{result}");
+            #endregion
+            #region q5
+            Console.Write("Enter a String : ");
+            string originalString = Console.ReadLine();
+            string reverseString = string.Empty;
+            for (int i = originalString.Length - 1; i >= 0; i--)
+            {
+                reverseString += originalString[i];
+            }
+            Console.Write($"Reverse String is : {reverseString} ");
+            Console.ReadLine();
             #endregion
         }
     }
