b53612299249cc5d296610fad28535f943b74123
ahmedzakaria2003 <you@example.com>
2024-12-23 01:11:27 +0200

    q3 is solved

--- Assignment-5-CSharp/Program.cs
+++ Assignment-5-CSharp/Program.cs
@@ -20,13 +20,37 @@
 
             #endregion
             #region q2
-            Console.WriteLine("Enter an integer: ");
-            int num = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= 12; i++) {
+            //Console.WriteLine("Enter an integer: ");
+            //int num = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= 12; i++) {
 
-                   Console.WriteLine($"{num} * {i} =   {num * i}  ");
+            //       Console.WriteLine($"{num} * {i} =   {num * i}  ");
+            //}
+
+            #endregion
+            #region q3
+
+            Console.WriteLine("Enter an integer: ");
+            int num;
+            if (int.TryParse(Console.ReadLine(), out num))
+            {
+                for (int i = 2; i <= num; i += 2)
+                {
+                    Console.Write(i + " ");
+
+                }
+
             }
 
+
+            #endregion
+            #region q4
+            //Console.WriteLine("enter first int");
+            //int number01 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("enter second int");
+            //int number02 = int.Parse(Console.ReadLine());
+            //double result = Math.Pow(number01, number02);
+            //Console.WriteLine($"{result}");
             #endregion
         }
     }
