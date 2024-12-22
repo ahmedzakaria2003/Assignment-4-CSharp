710065eb4f132c3c2778a9e43dd48e62d64e35be
ahmedzakaria2003 <you@example.com>
2024-12-23 01:27:27 +0200

    qq5 is solved

--- Assignment-5-CSharp/Program.cs
+++ Assignment-5-CSharp/Program.cs
@@ -53,16 +53,17 @@
             //Console.WriteLine($"{result}");
             #endregion
             #region q5
-            Console.Write("Enter a String : ");
-            string originalString = Console.ReadLine();
-            string reverseString = string.Empty;
-            for (int i = originalString.Length - 1; i >= 0; i--)
-            {
-                reverseString += originalString[i];
-            }
-            Console.Write($"Reverse String is : {reverseString} ");
-            Console.ReadLine();
+            //Console.Write("Enter a String : ");
+            //string originalString = Console.ReadLine();
+            //string reverseString = string.Empty;
+            //for (int i = originalString.Length - 1; i >= 0; i--)
+            //{
+            //    reverseString += originalString[i];
+            //}
+            //Console.Write($"Reverse String is : {reverseString} ");
+            //Console.ReadLine();
             #endregion
+
         }
     }
 }
