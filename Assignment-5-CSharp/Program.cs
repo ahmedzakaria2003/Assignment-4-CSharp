bfa4fcc637c5052d3d7ce2892a46e84cc4c39b24
ahmedzakaria2003 <you@example.com>
2024-12-23 00:55:43 +0200

    q2 is solved

--- Assignment-5-CSharp/Program.cs
+++ Assignment-5-CSharp/Program.cs
@@ -6,18 +6,27 @@
         {
             #region q1
 
+            //Console.WriteLine("Enter an integer: ");
+            //int num;
+            //if (int.TryParse(Console.ReadLine(), out num)) {
+            //    for (int i = 1; i <= num; i++)
+            //    {
+            //        Console.Write(i + (i < num ? ", " : "\n"));
+
+            //    }
+
+            //}
+
+
+            #endregion
+            #region q2
             Console.WriteLine("Enter an integer: ");
-            int num;
-            if (int.TryParse(Console.ReadLine(), out num)) {
-                for (int i = 1; i <= num; i++)
-                {
-                    Console.Write(i + (i < num ? ", " : "\n"));
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 12; i++) {
 
-                }
-
+                   Console.WriteLine($"{num} * {i} =   {num * i}  ");
             }
 
-            
             #endregion
         }
     }
